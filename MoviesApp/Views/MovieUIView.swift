9f441d1ff15d128cbe568a8b1bc8fdec4ff45b40
sambit-112081 <sambitprakash28@gmil.com>
2019-10-31 22:52:08 +0530

    - added macOS - button navigate

--- MoviesApp/Views/MovieUIView.swift
+++ MoviesApp/Views/MovieUIView.swift
@@ -11,72 +11,74 @@ import SwiftUI
 struct MovieUIView: View {
     var movie: Movie
     
+    @State var moveToTrailer: Bool = false
+    
     var body: some View {
-        VStack {
-            URLImageView(urlString: movie.poster)
-                .frame(minWidth: 250, maxWidth: .infinity, maxHeight: 300)
-            HStack(alignment: .top, spacing: 25) {
-                Button(action: {
-                    print("Add to list")
-                }) {
-                    Image(systemName: "list.bullet")
-                        .imageScale(.medium)
-                        .foregroundColor(.primary)
-                        .padding([.top, .leading, .trailing])
+        NavigationLink(destination: VideosView(videos: []), isActive: $moveToTrailer) {
+            VStack {
+                URLImageView(urlString: movie.poster)
+                    .frame(minWidth: 250, maxWidth: .infinity, maxHeight: 300)
+                HStack(alignment: .top, spacing: 25) {
+                    Button(action: {
+                        print("Add to list")
+                    }) {
+                        Image(systemName: "list.bullet")
+                            .imageScale(.medium)
+                            .foregroundColor(.primary)
+                            .padding([.top, .leading, .trailing])
+                            
+                        Text("Add to List")
+                            .foregroundColor(.primary)
+                    }
+                    
+                    Button(action: {
+                        print("favourite")
+                    }) {
+                        Image(systemName: "heart")
+                            .imageScale(.medium)
+                            .foregroundColor(.primary)
+                            .padding([.top, .leading, .trailing])
+                            
+                        Text("Favourite")
+                            .foregroundColor(.primary)
+                    }
+                    
+                    Button(action: {
+                        print("watch list")
+                    }) {
+                        Image(systemName: "bookmark")
+                            .imageScale(.medium)
+                            .foregroundColor(.primary)
+                            .padding([.top, .leading, .trailing])
                         
-                    Text("Add to List")
-                        .foregroundColor(.primary)
-                }
-                
-                Button(action: {
-                    print("favourite")
-                }) {
-                    Image(systemName: "heart")
-                        .imageScale(.medium)
-                        .foregroundColor(.primary)
-                        .padding([.top, .leading, .trailing])
+                        Text("Watch List")
+                            .foregroundColor(.primary)
+                    }
+                    
+                    Button(action: {
+                        self.moveToTrailer = true
                         
-                    Text("Favourite")
-                        .foregroundColor(.primary)
+                    }) {
+                        Image(systemName: "video")
+                            .imageScale(.medium)
+                            .foregroundColor(.primary)
+                            .padding([.top, .leading, .trailing])
+                        
+                        Text("Trailer")
+                            .foregroundColor(.primary)
+                    }
                 }
+                .frame(minWidth: 250, maxWidth: .infinity)
+                .padding(.horizontal)
                 
-                Button(action: {
-                    print("watch list")
-                }) {
-                    Image(systemName: "bookmark")
-                        .imageScale(.medium)
-                        .foregroundColor(.primary)
-                        .padding([.top, .leading, .trailing])
-                    
-                    Text("Watch List")
-                        .foregroundColor(.primary)
-                }
+                Text(movie.overview)
+                    .font(.body)
+                    .foregroundColor(.primary)
+                    .padding(.top)
+                    .padding(.horizontal, 16)
                 
-                Button(action: {
-                    print("trailer")
-//                        NavigationLink(destination: VideosView(videos: [])) {
-//                            Text("Trailer")
-//                        }
-                    
-                }) {
-                    Image(systemName: "video")
-                        .imageScale(.medium)
-                        .foregroundColor(.primary)
-                        .padding([.top, .leading, .trailing])
-                    
-                    Text("Trailer")
-                        .foregroundColor(.primary)
-                }.tag(4)
+                Spacer()
             }
-            .frame(minWidth: 250, maxWidth: .infinity)
-            .padding(.horizontal)
-            
-            Text(movie.overview)
-                .font(.body)
-                .padding(.top)
-                .padding(.horizontal, 16)
-            
-            Spacer()
         }
         .navigationBarTitle(movie.name)
     }
